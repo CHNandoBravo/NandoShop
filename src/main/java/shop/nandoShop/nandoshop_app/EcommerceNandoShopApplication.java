b9bf5b45574b12c0b69cdb7bf937f404942b1b4d
Nando Bravo CH <nando.bravo@changoconsultora.com>
2025-01-01 17:26:56 -0300

    primer commit

---
+++ src/main/java/shop/nandoShop/nandoshop_app/EcommerceNandoShopApplication.java
@@ -0,0 +1,13 @@
+package shop.nandoShop.nandoshop_app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcommerceNandoShopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EcommerceNandoShopApplication.class, args);
+	}
+
+}
---
+++ src/test/java/shop/nandoShop/nandoshop_app/EcommerceNandoShopApplicationTests.java
@@ -0,0 +1,13 @@
+package shop.nandoShop.nandoshop_app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcommerceNandoShopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
